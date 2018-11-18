1b562ff7d51e5c21ea35677dde9ecf8eeb1c3993
Bilal <mrbilal.programmer@gmail.com>
2018-11-18 18:58:46 +0500

    Update util.js

--- util.js
+++ util.js
@@ -1,1 +1,13 @@
 
+function range(multiple_of, limit, start) {
+  var increasing_number =  (start === undefined)  ? 0 : start;
+  var numbers_range = [];
+  for(;;) {
+    
+    increasing_number = increasing_number + multiple_of;
+    if (increasing_number > limit) return numbers_range;
+    numbers_range.push(increasing_number);
+     
+  }
+  
+}
