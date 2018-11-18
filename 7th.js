b238650ccc491d26be8b6c84717a73893dd75b2e
Bilal <mrbilal.programmer@gmail.com>
2018-11-18 21:45:21 +0500

    Update 7th.js

--- 7th.js
+++ 7th.js
@@ -1,1 +1,15 @@
 
+function prime_range(limit) {
+  
+  var odd_range = range(1, limit, 2).filter(v => (v % 2) === 1);
+  var primes = [];
+  for(;;) {
+     
+     var p = odd_range[0];
+     primes.push(p);
+     odd_range = odd_range.filter(v => (v % p !== 0));
+    
+     if ((p * p) > limit) return primes.concat(odd_range);
+  }
+}
+
